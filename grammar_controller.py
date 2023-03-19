d2ee439220ecca4f95ad0fa2aa825f785abc34a5
JohnSloan8 <johnsloan1984@gmail.com>
2023-03-19 17:55:19 +0000

    added collated Dictionary

--- grammar_controller.py
+++ grammar_controller.py
@@ -1,20 +1,36 @@
-from tokenise.create_word_object_list import create_word_object_list
-from grammar_check.check_article_noun_gender import check_article_noun_gender_number
+# from tokenise.create_word_object_list import create_word_object_list
+# from grammar_check.check_article_noun_gender import check_article_noun_gender_number
+import json
+
+
+with open("./wordData/wordObjectDictionary.json") as json_file:
+    wordObjectDictionary = json.load(json_file)
 
 def grammar_controller(phrase: str):
-    word_list = phrase.split()
-    word_objects_list = create_word_object_list(word_list)
-    # for now, just take the first word from each array
-    word_object_list = [x[0] for x in word_objects_list]
+    word_list = phrase.lower().split()
+    print('word_list:', word_list)
+
+    word_object_list = []
+    for word in word_list:
+        if word in wordObjectDictionary:
+            word_object_list.append(wordObjectDictionary[word])
+        else:
+            word_object_list.append([])
+
     print('word_object_list:', word_object_list)
-    article_noun_gender_number = check_article_noun_gender_number(word_object_list)
-    # print('article_noun_gender_number:', article_noun_gender_number)
-    corrected_word_list = word_list.copy()
-    for key in article_noun_gender_number.keys():
-        corrected_word_list[key] = article_noun_gender_number[key]
-    # # print('corrected_word_list:', corrected_word_list)
-    corrected_phrase = ' '.join(corrected_word_list)
-    return corrected_phrase
+
+    # word_objects_list = create_word_object_list(word_list)
+    # # for now, just take the first word from each array
+    # word_object_list = [x[0] for x in word_objects_list]
+    # print('word_object_list:', word_object_list)
+    # article_noun_gender_number = check_article_noun_gender_number(word_object_list)
+    # # print('article_noun_gender_number:', article_noun_gender_number)
+    # corrected_word_list = word_list.copy()
+    # for key in article_noun_gender_number.keys():
+    #     corrected_word_list[key] = article_noun_gender_number[key]
+    # # # print('corrected_word_list:', corrected_word_list)
+    # corrected_phrase = ' '.join(corrected_word_list)
+    # return corrected_phrase
 
 if __name__ == "__main__":
-    grammar_controller("an ádh")
+    grammar_controller("Chonaic mé an bhean bheag ag rith leis")
