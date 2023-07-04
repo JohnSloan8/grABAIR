1b9c66cbdcfe8f1c8e39d02b273e1764dda3de81
JohnSloan8 <johnsloan1984@gmail.com>
2023-07-04 22:23:39 +0100

    last changes

--- main_api.py
+++ main_api.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from grammar_controller import grammar_controller
-
+import time
 app = FastAPI()
 
 @app.get("/")
 async def root(phrase: str):
-    phrase_as_list = phrase.split()
-    grammar_controller(phrase_as_list)
+    start = time.time()
+    word_object_list = grammar_controller(phrase)
+    end = time.time()
+    return {
+        "time": end - start,
+        "word_object_list": word_object_list
+    }
